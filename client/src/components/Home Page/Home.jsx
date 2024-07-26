068deb3341e488bf00a349b60ce01f9103090391
t_yordanova <97800053+crystal-wolfer@users.noreply.github.com>
2024-07-27 02:28:18 +0300

    fix issue with successToast

--- client/src/components/Home Page/Home.jsx
+++ client/src/components/Home Page/Home.jsx
@@ -1,29 +1,45 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate  } from "react-router-dom";
+import { useState,  useEffect } from "react";
 
-import Hero from './Hero.jsx'
-import Market from './Market.jsx'
-import Feature from './Feature.jsx'
-import HowWorks from './HowWorks.jsx'
-import Customers from './Customers.jsx'
-import News from './News.jsx'
+import Hero from "./Hero.jsx";
+import Market from "./Market.jsx";
+import Feature from "./Feature.jsx";
+import HowWorks from "./HowWorks.jsx";
+import Customers from "./Customers.jsx";
+import News from "./News.jsx";
 // import BasicToast from '../Toast Components/BasicToast.jsx'
-import SuccessToast from '../Toast Components/SuccessToast.jsx'
+import SuccessToast from "../Toast Components/SuccessToast.jsx";
 // import ErrorToast from '../Toast Components/ErrorToast.jsx'
 
 export default function Home() {
-
   const location = useLocation();
-  const message = location.state?.message;
+  const navigate = useNavigate();
+  const [message, setMessage] = useState(location.state?.message || "");
 
-  return(
+  useEffect(() => {
+
+    if (location.state?.message) {
+      navigate(location.pathname, { replace: true, state: {} });
+    }
+    
+    if (message) {
+      const timer = setTimeout(() => {
+        setMessage("");
+      }, 5500); 
+
+      return () => clearTimeout(timer);
+    }
+  }, [message, location, navigate]);
+
+  return (
     <>
-     <Hero/>
-     <Market/>
-     <Feature />
-     <HowWorks />
-     <Customers />
-     <News />
-     {message && <SuccessToast message={message}/>}
+      <Hero />
+      <Market />
+      <Feature />
+      <HowWorks />
+      <Customers />
+      <News />
+      {message !=="" ? (<SuccessToast message={message}/>) : null}
     </>
-  )
+  );
 }
--- client/src/components/Toast Components/SuccessToast.jsx
+++ client/src/components/Toast Components/SuccessToast.jsx
@@ -14,7 +14,7 @@ export default function SuccessToast({message}) {
           <div className="flex flex-row items-center w-full">
             <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-800 bg-green-100 rounded-lg dark:bg-green-900 dark:text-green-300">
               <svg
-                class="w-5 h-5"
+                className="w-5 h-5"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -22,7 +22,7 @@ export default function SuccessToast({message}) {
               >
                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
               </svg>
-              <span class="sr-only">Check icon</span>
+              <span className="sr-only">Check icon</span>
             </div>
             <div className="ms-3 text-sm font-normal flex-1 text-left">
               {message}
@@ -44,9 +44,9 @@ export default function SuccessToast({message}) {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
                 />
               </svg>
