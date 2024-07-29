80c950bcdc134a9f1dd0ab36d5b9406180e36678
t_yordanova <97800053+crystal-wolfer@users.noreply.github.com>
2024-07-30 00:21:20 +0300

    small update

--- client/src/components/partials/DeleteOrderModal.jsx
+++ client/src/components/partials/DeleteOrderModal.jsx
@@ -55,7 +55,7 @@ function DeleteOrderModal({ show, onClose, coin }) {
               />
             </svg>
             <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-              Are you sure you want to remove this item?
+              Are you sure you want to remove {coin.name} order for $ {coin.paid}?
             </h3>
             <button
               data-modal-toggle="delete-modal"
